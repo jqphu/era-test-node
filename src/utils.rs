0fe83d43ed8dee0b08f27d3dc1eb07ef218e5c00
Justin Phu <me@justinphu.dev>
2024-01-27 13:12:04 +1100

    ✨ add events to DebugTraceCall

--- src/utils.rs
+++ src/utils.rs
@@ -230,6 +230,7 @@ pub fn create_debug_output(
             error: None,
             revert_reason: None,
             calls: traces.into_iter().map(Into::into).collect(),
+            events: result.logs.events.clone()
         }),
         ExecutionResult::Revert { output } => Ok(DebugCall {
             gas_used: result.statistics.gas_used.into(),
@@ -243,6 +244,7 @@ pub fn create_debug_output(
             error: None,
             revert_reason: Some(output.to_string()),
             calls: traces.into_iter().map(Into::into).collect(),
+            events: result.logs.events.clone()
         }),
         ExecutionResult::Halt { reason } => Err(Web3Error::SubmitTransactionError(
             reason.to_string(),
